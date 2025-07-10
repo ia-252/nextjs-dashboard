53216a114abeabafef412c962e00ad6c37bdfdc8
ia-252 <186896725+ia-252@users.noreply.github.com>
2025-07-10 14:19:14 +0900

    chapter14

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,11 +1,9 @@
-import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
-import { fetchInvoicesPages } from '@/app/lib/data';
 
  
 export default async function Page(props:{
@@ -17,8 +15,6 @@ export default async function Page(props:{
     const searchParams = await props.searchParams;
     const query = searchParams?.query || '';
     const currentPage = Number(searchParams?.page) || 1;
-    const totalPages = await fetchInvoicesPages(query);
-
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
